3a30646e714bb85d48f46f5aefc48241e11033bf
Jeffrey Linwood <jlinwood@gmail.com>
2012-03-29 15:48:33 -0500

    Change to GET

--- web.rb
+++ web.rb
@@ -5,11 +5,11 @@ get '/' do
   "Hello, world"
 end
 
-post '/twilio' do
+get '/twilio' do
   response = Twilio::TwiML::Response.new do |r|
   r.Sms 'hello there'
 end
 
 # print the result
-puts response.text
+response.text
 end
