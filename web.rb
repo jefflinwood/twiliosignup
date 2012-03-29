a8f28bce7cc4a17eb11d40b91ee98b290356759c
Jeffrey Linwood <jlinwood@gmail.com>
2012-03-29 15:45:49 -0500

    Added Twilio gem

--- web.rb
+++ web.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
   "Hello, world"
 end
 
-get '/twilio' do
-  "Hello Twilio"
+post '/twilio' do
+  response = Twilio::TwiML::Response.new do |r|
+  r.Sms 'hello there'
+end
+
+# print the result
+puts response.text
 end
