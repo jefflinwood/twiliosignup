5d3d6cf0e40f7493c2cab81b8e7eee88c3339e51
Jeffrey Linwood <jlinwood@gmail.com>
2012-04-10 20:47:59 -0500

    init

--- web.rb
+++ web.rb
@@ -7,9 +7,11 @@ end
 
 get '/twilio' do
   response = Twilio::TwiML::Response.new do |r|
-  r.Sms 'hello there'
+  	r.Sms 'You have subscribed to the RailsConf 2012 BohConf feed. Powered by Twilio. Text back STOP to leave'
+	end
+	response.text
 end
 
-# print the result
-response.text
+get '/send' do
+"Message Sent"
 end
