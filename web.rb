8bde291761009d9d640a4a21e1fc99b828ecbc59
Jeffrey Linwood <jlinwood@gmail.com>
2012-04-10 21:51:10 -0500

    Added Redis2Go SMS followers

--- web.rb
+++ web.rb
@@ -8,16 +8,29 @@ configure do
 end
 
 get '/' do
-  REDIS.get("dog")
+  	REDIS.scard "sms_followers"
 end
 
 get '/twilio' do
-  response = Twilio::TwiML::Response.new do |r|
+	if (params['Body'].include? "STOP") do
+		REDIS.srem "sms_followers", params['From']
+		response = Twilio::TwiML::Response.new do |r|
+  			r.Sms 'You have unsubscribed from the RailsConf 2012 BohConf feed. Powered by Twilio.'
+		end
+		response.text
+	end
+
+	REDIS.sadd "sms_followers", params['From']
+  	response = Twilio::TwiML::Response.new do |r|
   	r.Sms 'You have subscribed to the RailsConf 2012 BohConf feed. Powered by Twilio. Text back STOP to leave'
 	end
 	response.text
 end
 
-get '/send' do
-"Message Sent"
+get '/members' do
+	REDIS.smembers "sms_followers"
+end
+
+get '/clear' do
+	REDIS.del "sms_followers"
 end
